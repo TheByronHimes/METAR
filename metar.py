af78565bd1ad06f57d344655bfea18fc4ad31f12
Byron Himes <Byron Himes>
2016-03-25 20:46:54 -0500

    Basic layout begun

--- metar.py
+++ metar.py
@@ -5,3 +5,21 @@ DESCRIPTION: Takes a METAR reading as input and outputs the interpretation (mean
 FILES: metar.py
 '''
 
+def getReading():
+    reading = input("Enter METAR reading: ")
+    return reading
+
+def getReportType(metar):
+    pass
+
+def getStationID(metar):
+    pass
+
+def interpret(metar):
+    # Format is as follows:
+    # TYPE ID TIME WIND VIS WX SKY T/TD ALT REMARK
+
+
+if __name__ == "__main__":
+    metar_reading = getReading()
+    
