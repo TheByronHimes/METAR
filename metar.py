d9829709025be0bf8ea4810962c5c60518f657e8
Byron Himes <Byron Himes>
2016-03-28 16:14:02 -0500

    Added some of the functions for validating METAR fields

--- metar.py
+++ metar.py
@@ -9,16 +9,77 @@ FILES: metar.py
 fields = {}
 
 def getReading():
+    # this function prompts the user to enter the data
+    # will modify later to provide options for file input or CL input
     reading = input("Enter METAR reading: ")
     return reading
 
-def isReportType(key, d):
-    pass
+def isReportType(fText, key, d):
+    # Returns bool indicating whether the field is "Type of Report"
+    # Values: "METAR" or "SPECI"
+    if fText.upper() == "METAR" or fText.upper() == "SPECI":
+        d[key] = "Type of Report: "
+        return True
+    else:
+        return False
 
-def isStationID(key, d):
-    pass
 
+def isStationID(fText, key, d):
+    # Returns bool indicating whether the field is "Station ID"
+    # Format: AAAA (four alpha chars)
+    for char in fText:
+        if char.isalpha == False:
+            return False
+    else:
+        d[key] = "Station ID: "
+        return True
 
+def isDateTime(fText, key, d):
+    # Returns bool indicating whether the field is "Date and Time of Report"
+    # Format: DDTTttZ
+    # DD = day of the month (01 - 31)
+    # TT = hour of the report (00 - 23)
+    # tt = minute of the report (00 - 59)
+    # Z is a char literal which indicates Zulu time..
+
+    # Validate field text length
+    if len(fText) > 7 or len(fText) < :
+        return False
+
+    for i in fText[0:6]:
+        if i.isalpha() != True:
+            return False
+
+    # Cast expected date & time values to ints
+    date = int(fText[0:2])
+    hour = int(fText[2:4])
+    minute = int(fText[4:6])
+
+    # Ensure values fall within expected range
+    if fText.upper()[-1] == "Z":
+        if date >= 0 and date <= 31:
+            if hour >= 0 and hour <= 23:
+                if minute >= 0 and minute <= 59:
+                    d[key] = "Date and Time of Report: "
+                    return True
+    else:
+        return False
+
+def isReportModifier(fText, key, d):
+    # Returns bool indicating whether the field is "Report Modifier"
+    # Values: "AUTO" or "COR"
+    if fText == "AUTO" or fText == "COR":
+        d[key] = "Report Modifier: "
+        return True
+    else:
+        return False
+
+def isWindGroup(fText, key, d):
+    # Returns bool indicating whether the field is "Wind: "
+    # Format: dddff(f)GmmmKT_nnnVxxx
+    
+
+# Program entry:
 if __name__ == "__main__":
 
     # get raw metar reading
@@ -27,7 +88,3 @@ if __name__ == "__main__":
     # populate the fields dictionary
     for i in (0, len(metar_reading)):
         fields[i] = ""
-
-
-
-    
